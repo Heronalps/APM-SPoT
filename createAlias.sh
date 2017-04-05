fd425742b9b791c57725bc2f582684cf535423a6
michaelzhang <heronalps@gmail.com>
2017-04-05 00:42:28 -0700

    clean scripts

--- createAlias.sh
+++
@@ -1,7 +0,0 @@
-aws lambda create-alias \
---region us-west-2 \
---function-name HelloPython \
---description "sample alias" \
---function-version "\$LATEST" \
---name DEV \
---profile adminuser
--- createFunction.sh
+++
@@ -1,9 +0,0 @@
-aws lambda create-function \
-  --region us-west-2 \
-  --function-name HelloPython \
-  --zip-file fileb://hello_python.zip \
-  --role arn:aws:iam::444253827891:role/lambda_basic_execution \
-  --handler hello_python.my_handler \
-  --runtime python2.7 \
-  --timeout 15 \
-  --memory-size 512
