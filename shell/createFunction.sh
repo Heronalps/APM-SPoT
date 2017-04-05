2516fa3e354ee54b4b6ae4dca8aaea83e833ff77
michaelzhang <heronalps@gmail.com>
2017-04-05 00:31:10 -0700

    Add create and invoke function script

---
+++ shell/createFunction.sh
@@ -0,0 +1,10 @@
+aws lambda create-function \
+  --region us-west-2 \
+  --function-name HelloPython \
+  --zip-file "file://Users/michaelzhang/Downloads/APM-SPoT/hello_python.zip" \
+  --role arn:aws:iam::444253827891:role/lambda_basic_execution \
+  --handler hello_python.my_handler \
+  --runtime python2.7 \
+  --timeout 15 \
+  --memory-size 512
+  --profile default
